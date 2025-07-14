70c3a69d144e850c8b0daf642231fca4fc2b3845
rithikayy <rithika1226@gmail.com>
2025-07-13 23:35:20 -0400

    added api key to .env , fixed typo in sketchbook

--- src/sketchbook.py
+++ src/sketchbook.py
@@ -63,5 +63,5 @@ def sketchai(pic_path):
 
   happiness_score = mood_vals[0]*1 + mood_vals[1]*4 + mood_vals[2]*2 + mood_vals[3]*10 + mood_vals[4]*5 +mood_vals[5]*1 + mood_vals[6]*6
 
-  data = [date.today(), anger, disgust, fear, joy, neutral, sadness, surprise, round(happiness_score), "Your beatiful artwork!"]
+  data = [date.today(), anger, disgust, fear, joy, neutral, sadness, surprise, round(happiness_score), "Your beautiful artwork!"]
   st.session_state.df = add_to_df(data, st.session_state.df)
